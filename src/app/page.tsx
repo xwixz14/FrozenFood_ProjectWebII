4298ce4b6d76d8306bac27b242da1ef148d45d22
Kelompok Xampp <151417600+xwixz14@users.noreply.github.com>
2025-01-05 18:14:18 +0700

    DwiCahyoKuncoro(xwixz14) : Membuat halaman gallery dan sudah responsif di segala paltform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
+import Gallery from "@/components/gallery/page";
 import Menu from "@/components/menu/page";
 import Navbar from "@/components/navbar/page";
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    {/* taro header disini */}
+      <Navbar />
+      {/* taro header disini */}
 
-    {/* Taro about disini */}
+      {/* Taro about disini */}
 
-    
-    <Menu/>
-    
-     </>
-    
+      <Menu />
+      <Gallery />
+    </>
   );
-
-
 }
---
+++ src/components/gallery/page.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { Poppins, Roboto_Condensed } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const roboto_condensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["400"], // Note: weight should be an array here for consistency
+});
+function Gallery() {
+  return (
+    <>
+      <div
+        className="w-full pl-12 pr-12 pt-[25px] mt-25px mb-25px"
+        id="gallery"
+      >
+        <div className="mb-25px mt-35px">
+          <h1
+            className={`${poppins.className} font-bold text-center text-2xl text-color2`}
+          >
+            Gallery
+          </h1>
+        </div>
+        <div className="carousel rounded-box w-full h-96 ">
+          <div className="carousel-item ">
+            <img
+              src="/images/gallery/gallery1.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery2.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery3.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery4.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery5.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery6.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery7.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery8.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery9.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery10.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery11.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery12.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery13.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/gallery/gallery14.jpg"
+              alt="Burger"
+              className="w-[100%] h-[100%] rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Gallery;
