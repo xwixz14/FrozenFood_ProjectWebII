1ac75b99cfa9ea2820e0a83baf8391d8ba0325db
02septiaaa <syarinaseptia@gmail.com>
2025-01-10 22:01:36 +0700

    02septiaaa: merubah ukuran border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,13 @@ import Gallery from "@/components/gallery/page";
 import Menu from "@/components/menu/page";
 import Navbar from "@/components/navbar/page";
 import About from "@/components/about/page";
-import { Children } from "react";
+
 
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      {/* taro header disini */}
       <Header />
       <About />
 
--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -16,7 +16,7 @@ function Footer() {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.1019641459343!2d105.284838!3d-5.401434400000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40dbdf73add9c7%3A0xcae70f92b331e91a!2sNuju%20Coffee%20%26%20Space!5e0!3m2!1sid!2sid!4v1735023418502!5m2!1sid!2sid"
                   width="800"
                   height="300"
-                  style={{ border: 0 }}
+                  style={{ border: 0.2 }}
                   loading="lazy"
                 ></iframe>
               </Link>
